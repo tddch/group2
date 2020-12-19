eea2ac9ffa72ca106ca9f104e30935d1c7c10a18
tddch <2324114278@qq.com>
2020-12-20 00:48:45 +0800

    作业

--- day05/王彪/王彪项目/src/main/java/com/example/day_04/back/HomeBack.java
+++
@@ -1,7 +0,0 @@
-package com.example.day_04.back;
-
-public interface HomeBack {
-
-    void showdui(Object object);
-    void showcuo(String msg);
-}
--- day05/王彪/王彪项目/src/main/java/com/example/day_04/view/IView.java
+++
@@ -1,6 +0,0 @@
-package com.example.day_04.view;
-
-public interface IView {
-    void showdui(Object object);
-    void showcuo(String msg);
-}
