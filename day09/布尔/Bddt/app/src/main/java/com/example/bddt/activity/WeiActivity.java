02e33b8d6bb256112546268121db9102108d72cf
tddch <2324114278@qq.com>
2020-12-24 21:44:20 +0800

    布尔

---
+++ day09/布尔/Bddt/app/src/main/java/com/example/bddt/activity/WeiActivity.java
@@ -0,0 +1,16 @@
+package com.example.bddt.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bddt.R;
+
+public class WeiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wei);
+    }
+}
